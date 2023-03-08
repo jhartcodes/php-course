669a803544ff0d666f5b185f0ce28e81761c4d66
joel hart <joel@joels-iMac.local>
2023-03-07 20:40:59 -0800

    finished half of respsonsive children breadcrump component

--- themes/fictional-university-theme 2/header.php
+++ themes/fictional-university-theme 2/header.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <?php wp_head(); ?>
-  </head>
-  <body>
-  <header class="site-header">
-      <div class="container">
+</head>
 
-        <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
+<body>
+    <header class="site-header">
+        <div class="container">
 
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
-        <div class="site-header__menu group">
-          <nav class="main-navigation">
-            <ul>
-              <!-- because local could have multiple sites instead of/aboutus  for links i use php function site_url -->
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-              <li><a href="<?php site_url('/about-us') ?>">Programs</a></li>
-              <li><a href="<?php site_url('/about-us') ?>">Events</a></li>
-              <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
-            </ul>
-          </nav>
-          <div class="site-header__util">
-            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-          </div>
+            <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong>
+                    University</a></h1>
+
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
+                    aria-hidden="true"></i></span>
+            <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+            <div class="site-header__menu group">
+                <nav class="main-navigation">
+                    <ul>
+                        <!-- because local could have multiple sites instead of/aboutus  for links i use php function site_url -->
+                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                        <li><a href="<?php site_url('/about-us') ?>">Programs</a></li>
+                        <li><a href="<?php site_url('/about-us') ?>">Events</a></li>
+                        <li><a href="#">Campuses</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                </nav>
+                <div class="site-header__util">
+                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search"
+                            aria-hidden="true"></i></span>
+                </div>
+            </div>
         </div>
-      </div>
     </header>
--- themes/fictional-university-theme 2/page.php
+++ themes/fictional-university-theme 2/page.php
@@ -4,23 +4,31 @@
 // https://developer.wordpress.org/reference/functions/have_posts/  loop referencence.
   while(have_posts()) {
     the_post(); ?>
- <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url( <?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url( <?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title();?></h1>
         <div class="page-banner__intro">
-       <p>DONT FORGET TO REPLACE ME LATER </p>
+            <p>DONT FORGET TO REPLACE ME LATER </p>
         </div>
-      </div>
     </div>
+</div>
 
-    <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
-<!--
+<div class="container container--narrow page-section">
+
+    <?php
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home"
+                    aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span
+                class="metabox__main"><?php the_title(); ?></span></p>
+    </div>
+    <?php }
+    ?>
+
+    <!--
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
@@ -31,12 +39,12 @@
 
   -->
 
-      <div class="generic-content">
+    <div class="generic-content">
         <?php the_content()?>
-      </div>
     </div>
-    
-  <?php }
+</div>
+
+<?php }
 
   get_footer();
 
