6d6f722a5bfee22a33ee89ab72be4a9cbe126305
joel hart <joel@joels-iMac.local>
2023-03-08 14:40:29 -0800

    finished archive section

---
+++ themes/fictional-university-theme 2/archive.php
@@ -0,0 +1,47 @@
+<!-- This file is used to render pages that are sub catergories of Authors, Dates, Catergories   -->
+
+<?php 
+get_header();?>
+
+
+
+
+
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url( <?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <!-- you could do a loop that checks each archive and customize the h1 header instead -->
+        <h1 class="page-banner__title"> <?php the_archive_title()?></h1>
+        <div class="page-banner__intro">
+            <p> <?php the_archive_description()?> </p>
+        </div>
+    </div>
+</div>
+
+<div class="container container--narrow page-section">
+    <?php
+  while(have_posts()) {
+    the_post(); ?>
+    <div class="post-item">
+        <h2 class="headline headline--medium headline--post-title"><a
+                href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+        <div class="metabox">
+            <!-- Functions below display the author name (can change nickname), the day as day.month.year https://wordpress.org/documentation/article/customize-date-and-time-format/, catergory of the blog  -->
+            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
+                <?php echo get_the_category_list(', '); ?></p>
+        </div>
+
+        <div class="generic-content">
+            <?php the_excerpt(); ?>
+            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading</a></p>
+        </div>
+
+    </div>
+    <?php }
+    echo paginate_links()
+?>
+</div>
+<? get_footer();
+?>
