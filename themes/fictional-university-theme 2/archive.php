4c5786da96b27835510149cafc15cb2c7b3d9320
Joel <joel.d.hart1@gmail.com>
2023-03-08 19:24:26 -0800

    added custom plugins and events

--- themes/fictional-university-theme 2/archive.php
+++ themes/fictional-university-theme 2/archive.php
@@ -3,10 +3,6 @@
 <?php 
 get_header();?>
 
-
-
-
-
 <div class="page-banner">
     <div class="page-banner__bg-image"
         style="background-image: url( <?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
